ddf0ab266d9f7c1e2f5e4197c7cce5cc3e52f3c0
Daniel Witurna <daniel.witurna@gmail.com>
2013-10-31 15:13:31 +0100

    Added bats drawing and axis label / title.

--- Uebung_1/Main.m
+++ Uebung_1/Main.m
@@ -16,5 +16,24 @@ for j = 1 : 20
     watchFilledArea{j} = watchStatsDB{1,j}.FilledArea;
 end
 
+for j = 1 : 20
+    batArea{j} = batStatsDB{1,j}.Area;
+    batFilledArea{j} = batStatsDB{1,j}.FilledArea;
+end
+
 %-- Linien Art, r rot, s square
+%-- plot (x, y) 
+subplot(1, 2, 1);
 plot(cell2mat(watchArea), cell2mat(watchFilledArea), '--rs');
+
+title('Watches');
+xlabel('watchArea');
+ylabel('watchFilledArea');
+
+%--  Bats
+subplot(1, 2, 2);
+plot(cell2mat(batArea), cell2mat(batFilledArea), '--rs');
+
+title('Bats');
+xlabel('batArea');
+ylabel('batFilledArea');
