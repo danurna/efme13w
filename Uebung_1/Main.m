45b795167fb1c6577f8a29ced7ce45b21cbf29fd
Florian Groh <flo.groh@gmail.com>
2013-11-01 19:50:27 +0100

    Scatter plot for all combinations of fields

--- Uebung_1/Main.m
+++ Uebung_1/Main.m
@@ -2,4 +2,13 @@ clear;
 filenames = { 'watch', 'brick', 'crown', 'key', 'apple' }; % 'crown', 'brick', 'apple', 'key', 'fork', 'pencil', 'fountain', 'hammer', 'bone' };
 dataStruct = readImagesAndCalculateProps(filenames);
 %showHist(dataStruct, filenames, 'roundness');
-showScatter(dataStruct, filenames, 'roundness', 'formfactor');
+
+fields = {'formfactor', 'roundness', 'aspectratio', ...
+    'Solidity'};
+
+for i = 1:numel(fields)-1
+  for j = i+1:numel(fields)
+      showScatter(dataStruct, filenames, fields{i}, fields{j});
+      
+  end
+end
