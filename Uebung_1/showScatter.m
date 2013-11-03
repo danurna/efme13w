2d09e76609aa37cbc75b9278afb067ccfd70faf3
Florian Groh <flo.groh@gmail.com>
2013-11-03 13:51:03 +0100

    draw dots instead of circles

--- Uebung_1/showScatter.m
+++ Uebung_1/showScatter.m
@@ -1,6 +1,7 @@
 function showScatter(DATA, DATACLASSES, labelx, labely)
 
-    gscatter(DATA(:,1),DATA(:,2),DATACLASSES, 'mcbrg', 'o');
+    gscatter(DATA(:,1),DATA(:,2),DATACLASSES, 'mcbrg');
+    legend('Location','NorthWest');
     xlabel(labelx);
     ylabel(labely);
 
