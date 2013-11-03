66c42069093305aad683e645dde081310a3b5771
Florian Groh <flo.groh@gmail.com>
2013-11-03 13:37:09 +0100

    showscatter now only draws a plot with given colors and labels

--- Uebung_1/showScatter.m
+++ Uebung_1/showScatter.m
@@ -1,18 +1,7 @@
-function showScatter(dataStruct, classNames, prop1, prop2)
-    
-    numberOfClasses = size(classNames, 2);
-    c = ones(20*numberOfClasses, 2);
-    classifiers = cell(20*numberOfClasses, 1);
-    
-    for j = 1 : numberOfClasses
-        startValue = (j-1) * 20 + 1;
-        endValue = j * 20;
-        c( startValue:endValue, 1) = getFeatureVector( dataStruct.(classNames{j}), prop1);
-        c( startValue:endValue, 2) = getFeatureVector( dataStruct.(classNames{j}), prop2);
-        classifiers(startValue:endValue) = classNames(j);
-    end
+function showScatter(DATA, DATACLASSES, labelx, labely)
 
-    gscatter(c(:,1), c(:,2), classifiers, 'mcbrg', 'o');
-    xlabel(prop1);
-    ylabel(prop2);
+    gscatter(DATA(:,1),DATA(:,2),DATACLASSES, 'mcbrg', 'o');
+    xlabel(labelx);
+    ylabel(labely);
+
 end
