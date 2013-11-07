ac55aa98752161699e2f9b9d7dbc2bffe70466c7
Florian Groh <flo.groh@gmail.com>
2013-11-07 01:12:05 +0100

    besserer test von knn

--- Uebung_1/testKNN.m
+++ Uebung_1/testKNN.m
@@ -1,17 +1,36 @@
 function testKNN(TRAIN, TRAINCLASSES)
 
-k = [1,2,3,5,10,15,20,25];
-diff = zeros(1,numel(k));
+elements = size(TRAINCLASSES,1);
+k = 1:99;
+
+matC = repmat(TRAINCLASSES(1),[elements,1]);
+
+matlabtime = 0;
+mytime_ = 0;
 
 for i = 1 : numel(k);
     fprintf('k = %d\n',k(i));
     for j = 1:100
         ix = [1:j-1,j+1:100];
         
-        matC = knnclassify(TRAIN(ix,:),TRAIN,TRAINCLASSES,k(i));
-        myC = knn(TRAIN(ix,:),TRAIN,TRAINCLASSES,k(i));
-        diff(i) = diff(i) + nnz(~strcmp(matC,myC));
+        tic;
+        matC(j) = knnclassify(TRAIN(j,:),TRAIN(ix,:),TRAINCLASSES(ix),k(i));
+        matlabtime = matlabtime + toc;
+        
     end
-    fprintf('difference between matlab knn and our knn: %d\n\n',diff(i));
+    
+    
+    tic;
+    myC_ = knn(TRAIN,TRAIN,TRAINCLASSES,k(i),true);
+    mytime_ = mytime_ + toc;
+    
+    diffMatlab = 100*(nnz(~strcmp(matC,myC_))/elements);
+    diffTrain = 100*(nnz(~strcmp(TRAINCLASSES,myC_))/elements);
+    
+    fprintf('\tdifference between matlab & ours): %d%%\n',diffMatlab);
+    fprintf('\tdifference between trainSet & knn classifier): %d%%\n', diffTrain);
 end
+disp(strcat('My singlecall-time: ',num2str(mytime_)));
+disp(strcat('Matlab time: ',num2str(matlabtime)));
+
 end
