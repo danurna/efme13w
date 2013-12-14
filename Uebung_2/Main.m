f90e5854ed0c239d1a37a40a15481bb7c49740b4
Daniel Witurna <daniel.witurna@gmail.com>
2013-12-14 17:25:57 +0100

    no message

--- Uebung_2/Main.m
+++ Uebung_2/Main.m
@@ -10,7 +10,7 @@ testSets = cell(1);
 bestFeaturesPerSet = cell(1);
 globalBestFeatures = cell(1);
 
-trainFactors = [0.75 0.75 0.75];
+trainFactors = [0.9 0.7 0.5];
 numOfSets = numel(trainFactors);
 
 for i = 1:numOfSets
@@ -27,8 +27,8 @@ for i = 1:numOfSets
     
     dispstat(sprintf('Finding best features for Test Set %d',i),'keepthis');
     
-    bestFeaturesPerSet{i} = getBestColumns(TS,TSC,TR,TRC,'mahalanobis');
-    %bestFeaturesPerSet{i} = getBestColumns(TS,TSC,TR,TRC,'knn',1:30);
+    %bestFeaturesPerSet{i} = getBestColumns(TS,TSC,TR,TRC,'mahalanobis');
+    bestFeaturesPerSet{i} = getBestColumns(TS,TSC,TR,TRC,'knn',1:1);
     
     dispstat(sprintf('%s\n\n',repmat('-',1,37)),'keepthis','keepprev');
     
