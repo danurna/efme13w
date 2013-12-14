23446d3fd28ce3ccf44d86ae198fa831e735ab7c
Daniel Witurna <daniel.witurna@gmail.com>
2013-12-14 14:35:10 +0100

    Sort fix

--- Uebung_2/Main.m
+++ Uebung_2/Main.m
@@ -8,12 +8,12 @@ testSets = cell(1);
 bestFeaturesPerSet = cell(1);
 globalBestFeatures = cell(1);
 
-trainFactors = [0.9 0.7 0.5];
+trainFactors = [0.75 0.75 0.75];
 numOfSets = numel(trainFactors);
 
 for i = 1:numOfSets
     [TR, TRC, TS, TSC] = splitDataIntoTestAndTraining(TRAIN, TRAINCLASSES, ...
-                            trainFactors(i), 1);
+                            trainFactors(i), i);
     
     trainStruct.data = TR;
     trainStruct.class = TRC;
@@ -23,8 +23,8 @@ for i = 1:numOfSets
     testStruct.class = TSC;
     testSets{i} = testStruct;
     
-    bestFeaturesPerSet{i} = getBestColumns(TS,TSC,TR,TRC,'mahalanobis');
-    %BESTFEATURESPERSET{i} = getBestColumns(TS,TSC,TR,TRC,'knn',1:30);
+    %bestFeaturesPerSet{i} = getBestColumns(TS,TSC,TR,TRC,'mahalanobis');
+    bestFeaturesPerSet{i} = getBestColumns(TS,TSC,TR,TRC,'knn',1:20);
     
     if i ~= 1
         globalBestFeatures = intersect(globalBestFeatures,bestFeaturesPerSet{i}(:,4));
@@ -34,14 +34,18 @@ for i = 1:numOfSets
     
 end
 
-%Get minimum combination of Features, that classifies more than 95 per cent
-%correctly. => find shortest string in cell array
-%Source: http://www.mathworks.com/matlabcentral/answers/63551
+% Find the best (=least) combination of columns from intersected features.
+bestColumns = [];
+shortest = Inf;
 
-val = cellfun(@(x) numel(x),globalBestFeatures);
-out = globalBestFeatures(val==min(val));
-bestColumns = str2num(out{1}); %#ok<ST2NM>
-
+for i = 1:size(globalBestFeatures,1)
+    % Convert strings to values and compare the length
+    columnsToTest = [str2num(globalBestFeatures{i})];
+    if( size(columnsToTest, 2) < shortest )
+       bestColumns = columnsToTest;
+       shortest = size(bestColumns, 2);
+    end
+end
 
 disp('# of missclassified Elements');
 for i = 1 : numOfSets
