4d3eb5e6df82d1a21b854db1d4bfd87324539e16
Florian Groh <flo.groh@gmail.com>
2013-12-15 19:40:17 +0100

    doesn’t just operate with loocv now

--- Uebung_2/tryAndPlotEveryK.m
+++ Uebung_2/tryAndPlotEveryK.m
@@ -1,10 +1,10 @@
-function tryAndPlotEveryK(TRAIN, TRAINCLASSES)
+function tryAndPlotEveryK(TEST, TESTCLASSES, TRAIN, TRAINCLASSES)
 
 %Checking effectiveness of each k
 disp('#####################################');
 fprintf('%s\n\n','Checking effectiveness of each k');
 
-elements = numel(TRAINCLASSES);
+elements = numel(TESTCLASSES);
 
 effective = zeros(1,elements-1);
 tic;
@@ -12,19 +12,16 @@ for k = 1:elements-1
     
     fprintf('k = %2d',k);
     if k == 1
-       [knnCLASSES dist] = knn(TRAIN,TRAIN,TRAINCLASSES,k,true);
+       [knnCLASSES dist] = knn(TEST,TRAIN,TRAINCLASSES,k,isequal(TEST,TRAIN));
     else
-       knnCLASSES = knn(TRAIN,TRAIN,TRAINCLASSES,k,true, dist); 
+       knnCLASSES = knn(TEST,TRAIN,TRAINCLASSES,k,isequal(TEST,TRAIN), dist); 
     end
     
     
-    if isnumeric(knnCLASSES(1))
-        absolutDiff = nnz(~(TRAINCLASSES == knnCLASSES));
-    else
-        absolutDiff = nnz(~strcmp(TRAINCLASSES,knnCLASSES));
-    end
-    difference = absolutDiff/elements;
-    effective(k) = 1-(difference);
+    
+    absolutDiff = nnz((TESTCLASSES == knnCLASSES));
+    relative = absolutDiff/elements;
+    effective(k) = relative;
     
     fprintf('\t%s / %3.2f%% Correctly classified\n','done', 100*effective(k));
     
@@ -32,6 +29,7 @@ for k = 1:elements-1
 end
 toc;
 
+figure;
 plot(100*effective, 'b-o');
 title('Effectivness of different k''s');
 
