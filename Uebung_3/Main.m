efb00b1d1fa6429001ef9b5a6023f297ffd59490
Florian Groh <flo.groh@gmail.com>
2014-01-12 17:27:01 +0100

    pseudo refactoring

--- Uebung_3/Main.m
+++ Uebung_3/Main.m
@@ -1,20 +1,10 @@
-inputs = load('data/perceptrondata.dat');
-target1 = load('data/perceptrontarget1.dat');
-target2 = load('data/perceptrontarget2.dat');
+dispstat('','init');
 
-inputs = horzcat(ones(size(inputs,1),1),inputs);
-target1(target1 == 0) = -1;
-target2(target2 == 0) = -1;
+%----------------PERCEPTRON-----------------
+perceptron
 
-inputs = inputs';
 
-TWOBIT = [  1 1 1 1
-            0 1 0 1
-            0 0 1 1  ];
+%-----------------STROKES-------------------
+strokes
 
-AND = [-1, -1, -1, 1];
-OR = [-1, 1, 1, 1];
-XOR = [-1, 1, 1, -1];
 
-[w1, out1] = perco(inputs,target1,1000);
-%[w2, out2] = perco(inputs,target2,1000);
---
+++ Uebung_3/perceptron.m
@@ -0,0 +1,25 @@
+dispstat(repmat('#',1,30),'keepprev','keepthis');
+dispstat('PERCEPTRON','keepthis');
+
+
+inputs = load('data/perceptrondata.dat');
+target1 = load('data/perceptrontarget1.dat');
+target2 = load('data/perceptrontarget2.dat');
+
+inputs = horzcat(ones(size(inputs,1),1),inputs);
+target1(target1 == 0) = -1;
+target2(target2 == 0) = -1;
+
+inputs = inputs';
+
+TWOBIT = [  1 1 1 1
+            0 1 0 1
+            0 0 1 1  ];
+
+AND = [-1, -1, -1, 1];
+OR = [-1, 1, 1, 1];
+XOR = [-1, 1, 1, -1];
+
+[w1, out1] = perco(inputs,target1,1000);
+%[w2, out2] = perco(inputs,target2,1000);
+dispstat('Ergebnisse und so');
