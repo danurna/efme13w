48bdfc546f683ba63b07e354dac1f76228ea8d25
Florian Groh <flo.groh@gmail.com>
2014-01-12 15:44:49 +0100

    unfinished perco

--- Uebung_3/Main.m
+++ Uebung_3/Main.m
@@ -1,1 +1,10 @@
 inputs = load('data/perceptrondata.dat');
+target1 = load('data/perceptrontarget1.dat');
+target2 = load('data/perceptrontarget2.dat');
+
+inputs = horzcat(ones(size(inputs,1),1),inputs);
+target1(target1 == 0) = -1;
+target2(target2 == 0) = -1;
+
+
+perco(inputs,target1,1000);
---
+++ Uebung_3/perco.m
@@ -0,0 +1,23 @@
+function [W, OUTPUT] = perco(INPUT, TARGET, MAXEPOCH)
+
+N = size(INPUT,1);
+W = zeros(1,size(INPUT,2));
+gamma = 0.5;
+
+OUTPUT = zeros(size(TARGET));
+
+count = 0;
+while ( ~isequal(OUTPUT,TARGET) && count < MAXEPOCH )
+    for i = 1:N
+        unWeighted = INPUT(:,i)*TARGET(i);
+        OUTPUT(i) = W*unWeighted;
+        if OUTPUT(i) < 0
+            W = W + gamma*unWeighted;
+        end
+    end
+    
+    count = count +1;
+end
+
+
+end
