d8e955f840802c13be3ec62fee9bbd08d243f1a8
Florian Groh <flo.groh@gmail.com>
2014-01-12 18:10:17 +0100

    different results

--- Uebung_3/strokes.m
+++ Uebung_3/strokes.m
@@ -3,11 +3,24 @@ dispstat('STROKES','keepprev');
 
 load('data/strokefeatures.mat');
 
-[TEST TESTCLASS TRAIN TRAINCLASS] = splitDataIntoTestAndTraining( ...
+[TRAIN TRAINCLASSES TEST TESTCLASSES] = splitDataIntoTestAndTraining( ...
     features_class(:,1:20), ...
     features_class(:,21), ...
     0.8, ...
     1 ...
 );
 
+PERCOTRAIN = vertcat(ones(size(TRAINCLASSES))',TRAIN');
+wetDryClasses = TRAINCLASSES;
+wetDryClasses(TRAINCLASSES <= 3) = -1;
+wetDryClasses(TRAINCLASSES > 3) = 1;
+
 clearvars features_class;
+
+knnResult = knn(TEST(:,1:10), TRAIN(:,1:10), TRAINCLASSES, 1);
+nnz(knnResult == TESTCLASSES)
+
+mahalResult = mahalClassify(TEST(:,1:10), TRAIN(:,1:10), TRAINCLASSES, false);
+nnz(mahalResult == TESTCLASSES)
+
+[w, out] = perco(PERCOTRAIN,wetDryClasses,1000);
