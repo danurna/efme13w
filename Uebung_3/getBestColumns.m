3e27aa11f15dbd645064dfb7910e9ffe8285b3ba
Daniel Witurna <daniel.witurna@gmail.com>
2014-01-16 12:31:37 +0100

    added classes for best columns selection

---
+++ Uebung_3/getBestColumns.m
@@ -0,0 +1,81 @@
+function [bestCombinations mostEffective] = getBestColumns(TEST, TESTCLASSES, TRAIN, TRAINCLASSES, type, kInterval)
+
+
+colNum = size(TRAIN,2);
+featureColumns = 1:colNum;
+
+bestCombinations = cell(1,4);
+mostEffective = 0;
+
+totalPossibilities = 0;
+for i = 1:colNum
+    totalPossibilities = totalPossibilities + nchoosek(colNum,i);
+end
+
+done = 0;
+nextRow = 0;
+blub = 0;
+
+for i = 1:colNum
+    
+    chosenColumns = nchoosek(featureColumns,i);
+    possibilities = size(chosenColumns,1);
+    
+    for j = 1:possibilities
+        
+        
+        percentageDone = (j+done)/totalPossibilities*100;
+        if percentageDone >= blub*5;
+            dispstat(sprintf('%s: |%-20s| %2.0f%%',type, repmat('=',1,blub),percentageDone));
+            blub = blub+1;
+        end
+        
+        if strcmp(type,'mahalanobis')
+            k = 0;
+            mahalClasses = mahalClassify(...
+                            TEST(:,chosenColumns(j,:)), ...
+                            TRAIN(:,chosenColumns(j,:)), ...
+                            TRAINCLASSES);
+                        
+            absolutDiff = nnz((TESTCLASSES == mahalClasses));
+            difference = absolutDiff/numel(TESTCLASSES);
+            
+            effectiveness = difference;
+        elseif strcmp(type, 'perceptron')
+             k = 0;
+             TRAIN = horzcat(ones(size(TRAIN,1),1),TRAIN);
+             [W, OUTPUT, count] = perco(TRAIN(:,chosenColumns(j,:)), TEST(:,chosenColumns(j,:)), 300);
+             size(W)
+             size(OUTPUT)
+             count
+             
+            absolutDiff = nnz((TESTCLASSES == OUTPUT(:,1)));
+            difference = absolutDiff/numel(TESTCLASSES);
+            
+            effectiveness = difference;
+        else
+            [k effectiveness] = evaluateMostEffectiveK(...
+                TEST(:,chosenColumns(j,:)), ...
+                TESTCLASSES, ...
+                TRAIN(:,chosenColumns(j,:)), ...
+                TRAINCLASSES, kInterval);
+        end
+        
+        if effectiveness >= 0.5 %empirisch
+            if effectiveness > mostEffective
+                mostEffective = effectiveness;
+            end
+            nextRow = nextRow+1;
+            
+            bestCombinations{nextRow,1} = effectiveness;
+            bestCombinations{nextRow,2} = k;
+            bestCombinations{nextRow,3} = chosenColumns(j,:);
+            bestCombinations{nextRow,4} = num2str(chosenColumns(j,:),'%02d ');
+            
+        end
+    end
+    done = done + j;
+    
+end
+
+end
